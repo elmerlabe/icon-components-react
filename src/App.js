6f46877e2b028bf2afa57a567010be29ab6c470b
eruma94 <elmerlabe@gmail.com>
2022-09-06 13:10:22 +0800

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,66 @@
-import logo from './logo.svg';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.min.js';
 
-function App() {
+import Icons from './components/icons';
+import { useState } from 'react';
+
+
+const App = () => {
+
+  const [query, setQuery] = useState("")
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div className='container'>
+        <div className='row'>
+          <div className='col-md-12 mt-3'>
+            <button 
+              type='button'
+              data-bs-toggle="modal"
+              data-bs-target="#modalIcons"
+              className='btn btn-secondary'>
+              Open modal
+            </button>
+          </div>
+        </div>
+
+      </div>
+
+      <div className="modal fade" id="modalIcons" tabIndex={-1} aria-labelledby="exampleModalLabel" aria-hidden="true">
+        <div className="modal-dialog modal-lg">
+          <div className="modal-content">
+            <div className="modal-header">
+              <h5 className="modal-title" id="exampleModalLabel">Icons from <a className="text-dark" href='https://icons.getbootstrap.com/' target="_blank">Bootstrap</a></h5>
+              <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+            </div>
+            <div className="modal-body">
+              <input 
+                className='mb-3 form-control'
+                placeholder='Search'
+                onChange={(e) => setQuery(e.target.value)}
+              />
+              <ul className='list-unstyled list row row-cols-8 '>
+                {
+                  Icons.filter(icon =>{
+                    if (query ==='') {
+                      return icon;
+                    }
+                    else if (icon.name.toLowerCase().includes(query.toLowerCase())) {
+                      return icon;
+                    }
+                  }).map((icon) => (
+                    
+                    <li type="button" key={icon.id} className='me-3 mb-4 col' value={icon.name}>{icon.img}</li>
+                    
+                  ))
+                }
+              </ul>
+            </div>
+          </div>
+        </div>
+      </div>
+      
     </div>
   );
 }
---
+++ src/components/icons.js
@@ -0,0 +1,232 @@
+
+import { 
+    Facebook, 
+    Instagram, 
+    Twitter, 
+    Tiktok, 
+    Youtube, 
+    Linkedin, 
+    Github,
+    Google,
+    Pinterest,
+    Snapchat,
+    Whatsapp,
+    Telegram,
+    Messenger,
+    Spotify,
+    Dribbble,
+    Slack,
+    UsbDrive,
+    Skype,
+    Bag,
+    CameraVideo,
+    CloudArrowDown,
+    Person,
+    Share,
+    Headphones,
+    Reception4,
+    Phone,
+    MusicNoteBeamed,
+    VinylFill,
+    Cart2,
+    Shop,
+    Envelope,
+    GeoAlt,
+    CloudFill,
+    Globe,
+    Camera,
+    CreditCard,
+    Git
+ 
+} from "react-bootstrap-icons"
+
+const size = 40;
+const color = "#3d3d3d";
+
+const Icons = [
+    {
+        id: 1,
+        name: "facebook",
+        img: <Facebook color={color} size={size}/>
+    },
+    {
+        id: 2,
+        name: "instagram",
+        img: <Instagram color={color} size={size}/>
+    },
+    {
+        id: 3,
+        name: "twitter",
+        img: <Twitter color={color} size={size}/>
+    },
+    {
+        id: 4,
+        name: "tiktok",
+        img: <Tiktok color={color} size={size}/>
+    },
+    {
+        id: 5,
+        name: "youtube",
+        img: <Youtube color={color} size={size}/>
+    },
+    {
+        id: 6,
+        name: "linkedin",
+        img: <Linkedin color={color} size={size}/>
+    },
+    {
+        id: 7,
+        name: "github",
+        img: <Github color={color} size={size}/>
+    },
+    {
+        id: 8,
+        name: "google",
+        img: <Google color={color} size={size}/>
+    },
+    {
+        id: 9,
+        name: "pinterest",
+        img: <Pinterest color={color} size={size}/>
+    },
+    {
+        id: 10,
+        name: "snapchat",
+        img: <Snapchat color={color} size={size}/>
+    },
+    {
+        id: 11,
+        name: "whatsapp",
+        img: <Whatsapp color={color} size={size}/>
+    },
+    {
+        id: 12,
+        name: "telegram",
+        img: <Telegram color={color} size={size}/>
+    },
+    {
+        id: 13,
+        name: "messenger",
+        img: <Messenger color={color} size={size}/>
+    },
+    {
+        id: 14,
+        name: "spotify",
+        img: <Spotify color={color} size={size}/>
+    },
+    {
+        id: 15,
+        name: "dribbble",
+        img: <Dribbble color={color} size={size}/>
+    },
+    {
+        id: 16,
+        name: "slack",
+        img: <Slack color={color} size={size}/>
+    },
+    {
+        id: 17,
+        name: "usbDrive",
+        img: <UsbDrive color={color} size={size}/>
+    },
+    {
+        id: 18,
+        name: "skype",
+        img: <Skype color={color} size={size}/>
+    },
+    {
+        id: 19,
+        name: "bag",
+        img: <Bag color={color} size={size}/>
+    },
+    {
+        id: 20,
+        name: "cameraVideo",
+        img: <CameraVideo color={color} size={size}/>
+    },
+    {
+        id: 21,
+        name: "cloudArrowDown",
+        img: <CloudArrowDown color={color} size={size}/>
+    },
+    {
+        id: 22,
+        name: "person",
+        img: <Person color={color} size={size}/>
+    },
+    {
+        id: 23,
+        name: "share",
+        img: <Share color={color} size={size}/>
+    },
+    {
+        id: 24,
+        name: "headphones",
+        img: <Headphones color={color} size={size}/>
+    },
+    {
+        id: 25,
+        name: "reception4",
+        img: <Reception4 color={color} size={size}/>
+    },
+    {
+        id: 26,
+        name: "phone",
+        img: <Phone color={color} size={size}/>
+    },
+    {
+        id: 27,
+        name: "musicNoteBeamed",
+        img: <MusicNoteBeamed color={color} size={size}/>
+    },
+    {
+        id: 28,
+        name: "vinylFill",
+        img: <VinylFill color={color} size={size}/>
+    },
+    {
+        id: 29,
+        name: "cart2",
+        img: <Cart2 color={color} size={size}/>
+    },
+    {
+        id: 30,
+        name: "shop",
+        img: <Shop color={color} size={size}/>
+    },
+    {
+        id: 31,
+        name: "envelope",
+        img: <Envelope color={color} size={size}/>
+    },
+    {
+        id: 32,
+        name: "geoAlt",
+        img: <GeoAlt color={color} size={size}/>
+    },
+    {
+        id: 33,
+        name: "cloudFill",
+        img: <CloudFill color={color} size={size}/>
+    },
+    {
+        id: 34,
+        name: "globe",
+        img: <Globe color={color} size={size}/>
+    },
+    {
+        id: 35,
+        name: "camera",
+        img: <Camera color={color} size={size}/>
+    },
+    {
+        id: 36,
+        name: "creditCard",
+        img: <CreditCard color={color} size={size}/>
+    }
+
+]
+
+export default Icons;
+
+
